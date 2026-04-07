24ca2548bdd5596009fa3f6549db48c4306aa1e2
Paula23cardenas <150209140+Paula23cardenas@users.noreply.github.com>
2026-04-07 15:23:40 -0500

    Fix typo in hamburger size from 'mediana' to 'mediano'

--- hamburguesa/src/main/java/com/sena/App.java
+++ hamburguesa/src/main/java/com/sena/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) {
 
         Hamburguesa h1 = new Hamburguesa("integral", true, true, true, true, true, "grande");
-        Hamburguesa h2 = new Hamburguesa("blanco", false, false, false, false, false, "mediana");
+        Hamburguesa h2 = new Hamburguesa("blanco", false, false, false, false, false, "mediano");
 
         System.out.println(h1.mostrarPedido());
         System.out.println(h2.mostrarPedido());
