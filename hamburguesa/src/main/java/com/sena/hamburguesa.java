8112b8eafcda527660bdc47dad88b85509504c47
Paula23cardenas <150209140+Paula23cardenas@users.noreply.github.com>
2026-04-06 20:49:00 -0500

    Enhance Hamburguesa class with new features

--- hamburguesa/src/main/java/com/sena/hamburguesa.java
+++ hamburguesa/src/main/java/com/sena/hamburguesa.java
@@ -5,20 +5,43 @@ public class Hamburguesa {
     private String tipoPan;
     private boolean dobleCarne;
     private boolean conSalsas;
+    private boolean conQueso;
+    private boolean conTocineta;
+    private boolean conVerduras;
+    private String tamano;
 
-    public Hamburguesa(String tipoPan, boolean dobleCarne, boolean conSalsas) {
+    public Hamburguesa(String tipoPan, boolean dobleCarne, boolean conSalsas,
+                       boolean conQueso, boolean conTocineta, boolean conVerduras, String tamano) {
         this.tipoPan = tipoPan;
         this.dobleCarne = dobleCarne;
         this.conSalsas = conSalsas;
+        this.conQueso = conQueso;
+        this.conTocineta = conTocineta;
+        this.conVerduras = conVerduras;
+        this.tamano = tamano;
     }
 
     public String mostrarPedido() {
-        String pedido = "Hamburguesa con pan " + tipoPan;
+        String pedido = "Hamburguesa " + tamano + " con pan " + tipoPan;
 
         if (dobleCarne) {
             pedido += ", doble carne";
         } else {
             pedido += ", carne sencilla";
+        }
+
+        if (conQueso) {
+            pedido += ", con queso";
+        }
+
+        if (conTocineta) {
+            pedido += ", con tocineta";
+        }
+
+        if (conVerduras) {
+            pedido += ", con verduras";
+        } else {
+            pedido += ", sin verduras";
         }
 
         if (conSalsas) {
